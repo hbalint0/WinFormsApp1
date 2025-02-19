4bab9efa97f4bffe03837632a913d0302c2552a1
2021130 <2021130@B104-06>
2025-02-19 07:02:41 +0100

    Add project files.

---
+++ WinFormsApp1/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace WinFormsApp1
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            kattint = new Button();
+            SuspendLayout();
+            // 
+            // kattint
+            // 
+            kattint.Location = new Point(261, 160);
+            kattint.Name = "kattint";
+            kattint.Size = new Size(174, 58);
+            kattint.TabIndex = 0;
+            kattint.Text = "Kattints rám!";
+            kattint.UseVisualStyleBackColor = true;
+            kattint.Click += kattint_Click;
+            kattint.MouseLeave += kattint_MouseLeave;
+            kattint.MouseHover += kattint_MouseHover;
+            // 
+            // MainForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(kattint);
+            Name = "MainForm";
+            Text = "Teszt";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button kattint;
+    }
+}
---
+++ WinFormsApp1/Form1.cs
@@ -0,0 +1,52 @@
+namespace WinFormsApp1
+{
+    public partial class MainForm : Form
+    {
+        private bool iranyFel = true, iranyBalra = true;
+        private int lepesFel = 20, lepesOldalt =30;
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void kattint_Click(object sender, EventArgs e)
+        {
+            Point point = kattint.Location;
+            if(iranyFel && (point.Y - lepesFel) <0)
+            {
+                iranyFel=false;
+            }
+            if (iranyBalra && (point.X - lepesOldalt) < 0)
+            {
+                iranyBalra = false;
+            }
+
+            if (!iranyFel && (point.Y - kattint.Height) + lepesFel < Height)
+            {
+                iranyFel=false;
+               
+            }
+
+            if (iranyBalra && (point.X - kattint.Width) + lepesOldalt < Width)
+            {
+                iranyBalra=true;
+                
+            }
+            point.X += iranyBalra ? -lepesOldalt : lepesOldalt;
+            point.Y += iranyFel ? -lepesFel : lepesFel;
+
+            kattint.Location = point;
+        }
+        private void kattint_MouseHover(object sender, EventArgs e)
+        {
+            kattint.BackColor = Color.Blue;
+            
+        }
+        private void kattint_MouseLeave(object sender, EventArgs e)
+        {
+            kattint.BackColor = Color.White;
+            kattint.Text = "Kattints rám!";
+        }
+       
+    }
+}
